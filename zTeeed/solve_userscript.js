d606e4dd48f01d5d78161b0a78a8d54c5a6f5e86
zteeed <zteeed@minet.net>
2019-05-09 10:27:19 +0200

    Remove comments

--- zTeeed/solve_userscript.js
+++ zTeeed/solve_userscript.js
@@ -19,7 +19,7 @@
         var mathPow = Math.pow;
         var maxWord = mathPow(2, 32);
         var lengthProperty = 'length'
-        var i, j; // Used as a counter across the whole file
+        var i, j;
         var result = ''
 
         var words = [];
@@ -38,17 +38,17 @@
             }
         }
 
-        ascii += '\x80' // Append Ƈ' bit (plus zero padding)
-        while (ascii[lengthProperty]%64 - 56) ascii += '\x00' // More zero padding
+        ascii += '\x80'
+        while (ascii[lengthProperty]%64 - 56) ascii += '\x00'
         for (i = 0; i < ascii[lengthProperty]; i++) {
             j = ascii.charCodeAt(i);
-            if (j>>8) return; // ASCII check: only accept characters in range 0-255
+            if (j>>8) return;
             words[i>>2] |= j << ((3 - i)%4)*8;
         }
         words[words[lengthProperty]] = ((asciiBitLength/maxWord)|0);
         words[words[lengthProperty]] = (asciiBitLength)
         for (j = 0; j < words[lengthProperty];) {
-            var w = words.slice(j, j += 16); // The message is expanded into 64 words as part of the iteration
+            var w = words.slice(j, j += 16); 
             var oldHash = hash;
             hash = hash.slice(0, 8);
 
@@ -57,20 +57,20 @@
                 var w15 = w[i - 15], w2 = w[i - 2];
                 var a = hash[0], e = hash[4];
                 var temp1 = hash[7]
-                + (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) // S1
-                + ((e&hash[5])^((~e)&hash[6])) // ch
+                + (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) 
+                + ((e&hash[5])^((~e)&hash[6]))
                 + k[i]
                 + (w[i] = (i < 16) ? w[i] : (
                     w[i - 16]
-                    + (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15>>>3)) // s0
+                    + (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15>>>3)) 
                     + w[i - 7]
-                    + (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2>>>10)) // s1
+                    + (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2>>>10)) 
                 )|0
                   );
-                var temp2 = (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) // S0
-                + ((a&hash[1])^(a&hash[2])^(hash[1]&hash[2])); // maj
+                var temp2 = (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) 
+                + ((a&hash[1])^(a&hash[2])^(hash[1]&hash[2])); 
 
-                hash = [(temp1 + temp2)|0].concat(hash); // We don't bother trimming off the extra ones, they're harmless as long as we're truncating when we do the slice()
+                hash = [(temp1 + temp2)|0].concat(hash); 
                 hash[4] = (hash[4] + temp1)|0;
             }
 
